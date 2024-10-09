52f260befb19c1a7942ba37bf3477e788c1eafb5
Lev Vereshchagin <mail@vrslev.com>
2024-10-09 14:40:55 +0300

    Add env-var aliases to improve compatibility with existing internal services and pipelines

--- microbootstrap/settings.py
+++ microbootstrap/settings.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import os
 import typing
 
+import pydantic
 import pydantic_settings
 
 from microbootstrap import (
@@ -16,7 +17,8 @@ from microbootstrap import (
 
 
 SettingsT = typing.TypeVar("SettingsT", bound="BaseServiceSettings")
-ENVIRONMENT_PREFIX: typing.Final = "ENVIRONMENT_PREFIX"
+ENV_PREFIX_VAR_NAME: typing.Final = "ENVIRONMENT_PREFIX"
+ENV_PREFIX: typing.Final = os.getenv(ENV_PREFIX_VAR_NAME, "")
 
 
 # TODO: add offline docs and cors support  # noqa: TD002
@@ -25,9 +27,13 @@ class BaseServiceSettings(
 ):
     service_debug: bool = True
     service_environment: str | None = None
-    service_name: str = "micro-service"
+    service_name: str = pydantic.Field(
+        "micro-service", validation_alias=pydantic.AliasChoices("SERVICE_NAME", f"{ENV_PREFIX}SERVICE_NAME")
+    )
     service_description: str = "Micro service description"
-    service_version: str = "1.0.0"
+    service_version: str = pydantic.Field(
+        "1.0.0", validation_alias=pydantic.AliasChoices("CI_COMMIT_TAG", f"{ENV_PREFIX}SERVICE_VERSION")
+    )
     service_static_path: str = "/static"
 
     server_host: str = "0.0.0.0"  # noqa: S104
@@ -37,7 +43,7 @@ class BaseServiceSettings(
 
     model_config = pydantic_settings.SettingsConfigDict(
         env_file=".env",
-        env_prefix=os.getenv(ENVIRONMENT_PREFIX, ""),
+        env_prefix=ENV_PREFIX,
         env_file_encoding="utf-8",
         populate_by_name=True,
     )
