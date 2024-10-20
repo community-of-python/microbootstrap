93ac0bb56a31ed0c60730b62efefaae22f71a190
reqww <my.gurbanov@gmail.com>
2024-10-20 15:22:39 +0300

    Add health checks support

---
+++ microbootstrap/instruments/health_checks_instrument.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from health_checks.http_based import DefaultHTTPHealthCheck
+
+from microbootstrap.instruments.base import BaseInstrumentConfig, Instrument
+
+
+class HealthChecksConfig(BaseInstrumentConfig):
+    service_name: str = "micro-service"
+    service_version: str = "1.0.0"
+
+    health_checks_enabled: bool = True
+    health_checks_path: str = "/health/"
+
+
+class HealthChecksInstrument(Instrument[HealthChecksConfig]):
+    instrument_name = "Health checks"
+    ready_condition = "Set health_checks_enabled to True"
+
+    def bootstrap(self) -> None:
+        self.health_check = DefaultHTTPHealthCheck(
+            service_version=self.instrument_config.service_version,
+            service_name=self.instrument_config.service_name,
+        )
+
+    def is_ready(self) -> bool:
+        return self.instrument_config.health_checks_enabled
+
+    @classmethod
+    def get_config_type(cls) -> type[HealthChecksConfig]:
+        return HealthChecksConfig
---
+++ tests/instruments/test_health_checks.py
@@ -0,0 +1,71 @@
+import typing
+
+import fastapi
+import litestar
+from httpx import AsyncClient
+from litestar import status_codes
+from litestar.testing import AsyncTestClient
+
+from microbootstrap.bootstrappers.fastapi import FastApiHealthChecksInstrument
+from microbootstrap.bootstrappers.litestar import LitestarHealthChecksInstrument
+from microbootstrap.instruments.health_checks_instrument import (
+    HealthChecksConfig,
+    HealthChecksInstrument,
+)
+
+
+def test_health_checks_is_ready(minimal_health_checks_config: HealthChecksConfig) -> None:
+    health_checks_instrument: typing.Final = HealthChecksInstrument(minimal_health_checks_config)
+    assert health_checks_instrument.is_ready()
+
+
+def test_health_checks_bootstrap_is_not_ready(minimal_health_checks_config: HealthChecksConfig) -> None:
+    minimal_health_checks_config.health_checks_enabled = False
+    health_checks_instrument: typing.Final = HealthChecksInstrument(minimal_health_checks_config)
+    assert not health_checks_instrument.is_ready()
+
+
+def test_health_checks_bootstrap_after(
+    default_litestar_app: litestar.Litestar,
+    minimal_health_checks_config: HealthChecksConfig,
+) -> None:
+    health_checks_instrument: typing.Final = HealthChecksInstrument(minimal_health_checks_config)
+    assert health_checks_instrument.bootstrap_after(default_litestar_app) == default_litestar_app
+
+
+def test_health_checks_teardown(
+    minimal_health_checks_config: HealthChecksConfig,
+) -> None:
+    health_checks_instrument: typing.Final = HealthChecksInstrument(minimal_health_checks_config)
+    assert health_checks_instrument.teardown() is None  # type: ignore[func-returns-value]
+
+
+async def test_litestar_health_checks_bootstrap() -> None:
+    test_health_checks_path: typing.Final = "/test-path/"
+    heatlh_checks_config: typing.Final = HealthChecksConfig(health_checks_path=test_health_checks_path)
+    health_checks_instrument: typing.Final = LitestarHealthChecksInstrument(heatlh_checks_config)
+
+    health_checks_instrument.bootstrap()
+    litestar_application: typing.Final = litestar.Litestar(
+        **health_checks_instrument.bootstrap_before(),
+    )
+
+    async with AsyncTestClient(app=litestar_application) as async_client:
+        response = await async_client.get(heatlh_checks_config.health_checks_path)
+        assert response.status_code == status_codes.HTTP_200_OK
+
+
+async def test_fastapi_health_checks_bootstrap() -> None:
+    test_health_checks_path: typing.Final = "/test-path/"
+    heatlh_checks_config: typing.Final = HealthChecksConfig(health_checks_path=test_health_checks_path)
+    health_checks_instrument: typing.Final = FastApiHealthChecksInstrument(heatlh_checks_config)
+
+    health_checks_instrument.bootstrap()
+    fastapi_application = fastapi.FastAPI(
+        **health_checks_instrument.bootstrap_before(),
+    )
+    fastapi_application = health_checks_instrument.bootstrap_after(fastapi_application)
+
+    async with AsyncClient(app=fastapi_application, base_url="http://testserver") as async_client:
+        response = await async_client.get(heatlh_checks_config.health_checks_path)
+        assert response.status_code == status_codes.HTTP_200_OK
