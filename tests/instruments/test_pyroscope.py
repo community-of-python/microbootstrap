f5b211f68d4821736e1921e6c206e612bd3ec02a
Lev Vereshchagin <mail@vrslev.com>
2025-04-18 12:41:19 +0300

    Add OpentelemetryConfig and PyroscopeSpanProcessor to test_pyroscope.py

--- tests/instruments/test_pyroscope.py
+++ tests/instruments/test_pyroscope.py
@@ -1,6 +1,8 @@
 import pydantic
 import pytest
+from pyroscope.otel import PyroscopeSpanProcessor  # type: ignore[import-untyped]
 
+from microbootstrap.instruments.opentelemetry_instrument import OpentelemetryConfig, OpentelemetryInstrument
 from microbootstrap.instruments.pyroscope_instrument import PyroscopeConfig, PyroscopeInstrument
 
 
@@ -18,3 +20,13 @@ class TestPyroscopeInstrument:
     def test_not_ready(self) -> None:
         instrument = PyroscopeInstrument(PyroscopeConfig(pyroscope_endpoint=None))
         assert not instrument.is_ready()
+
+    def test_opentelemetry_includes_pyroscope(self) -> None:
+        otel_instrument = OpentelemetryInstrument(
+            OpentelemetryConfig(pyroscope_endpoint=pydantic.HttpUrl("http://localhost:4040"))
+        )
+        otel_instrument.bootstrap()
+        assert PyroscopeSpanProcessor in {
+            type(one_span_processor)
+            for one_span_processor in otel_instrument.tracer_provider._active_span_processor._span_processors  # noqa: SLF001
+        }
