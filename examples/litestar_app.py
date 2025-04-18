b15ad7c279b8699246c9f1ac33da7798e14130fd
Lev Vereshchagin <mail@vrslev.com>
2025-04-18 13:02:57 +0300

    Adding Litestar example application and updating pyproject.toml for ignore rules

---
+++ examples/litestar_app.py
@@ -0,0 +1,23 @@
+import litestar
+
+from microbootstrap import LitestarSettings
+from microbootstrap.bootstrappers.litestar import LitestarBootstrapper
+from microbootstrap.config.litestar import LitestarConfig
+from microbootstrap.granian_server import create_granian_server
+
+
+@litestar.get("/")
+async def hello_world() -> dict[str, str]:
+    return {"hello": "world"}
+
+
+def create_app() -> litestar.Litestar:
+    return (
+        LitestarBootstrapper(LitestarSettings(pyroscope_endpoint="http://localhost:4040"))
+        .configure_application(LitestarConfig(route_handlers=[hello_world]))
+        .bootstrap()
+    )
+
+
+if __name__ == "__main__":
+    create_granian_server("t:create_app", LitestarSettings(), factory=True).serve()
