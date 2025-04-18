217cdf79b02f89d750b387e9dd3be99435f87ba8
Lev Vereshchagin <mail@vrslev.com>
2025-04-18 11:50:16 +0300

    Add Pyroscope instrumentation configuration and implementation

---
+++ microbootstrap/instruments/pyroscope_instrument.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+import pyroscope_io  # type: ignore[import-untyped]
+
+from microbootstrap.instruments.base import BaseInstrumentConfig, Instrument
+
+
+if TYPE_CHECKING:
+    import pydantic
+
+
+class PyroscopeConfig(BaseInstrumentConfig):
+    service_name: str = "micro-service"
+
+    pyroscope_endpoint: pydantic.HttpUrl | None = None
+    pyroscope_sample_rate: int = 100
+
+
+class PyroscopeInstrument(Instrument[PyroscopeConfig]):
+    instrument_name = "Pyroscope"
+    ready_condition = "Provide endpoint"
+
+    def is_ready(self) -> bool:
+        return all([self.instrument_config.pyroscope_endpoint, self.instrument_config.pyroscope_sample_rate])
+
+    def teardown(self) -> None:
+        pyroscope_io.shutdown()
+
+    def bootstrap(self) -> None:
+        pyroscope_io.configure(
+            application_name=self.instrument_config.service_name,
+            server_address=self.instrument_config.pyroscope_endpoint,
+            sample_rate=self.instrument_config.pyroscope_sample_rate,
+        )
+
+    @classmethod
+    def get_config_type(cls) -> type[PyroscopeConfig]:
+        return PyroscopeConfig
