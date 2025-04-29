6435ca48e2cc94810fa23697bc1ea8c24cd82b47
Lev Vereshchagin <mail@vrslev.com>
2025-04-29 19:25:31 +0300

    Set `span_name` tag on Pyroscope profiles (#96)
    * Enhance Pyroscope integration by customizing span processor and updating dependencies
    
    * Add type ignore comment for untyped pyroscope import
    
    * Adjust span name key and update test for Pyroscope instrumentation

--- tests/instruments/test_pyroscope.py
+++ tests/instruments/test_pyroscope.py
@@ -1,12 +1,19 @@
+import typing
+from unittest import mock
+from unittest.mock import Mock
+
+import fastapi
 import pydantic
 import pytest
+from fastapi.testclient import TestClient as FastAPITestClient
 
-from microbootstrap.instruments.opentelemetry_instrument import OpentelemetryConfig, OpentelemetryInstrument
+from microbootstrap.bootstrappers.fastapi import FastApiOpentelemetryInstrument
+from microbootstrap.instruments.opentelemetry_instrument import OpentelemetryConfig
 from microbootstrap.instruments.pyroscope_instrument import PyroscopeConfig, PyroscopeInstrument
 
 
 try:
-    from pyroscope.otel import PyroscopeSpanProcessor  # type: ignore[import-untyped]
+    import pyroscope  # type: ignore[import-untyped]  # noqa: F401
 except ImportError:  # pragma: no cover
     pytest.skip("pyroscope is not installed", allow_module_level=True)
 
@@ -26,12 +33,26 @@ class TestPyroscopeInstrument:
         instrument = PyroscopeInstrument(PyroscopeConfig(pyroscope_endpoint=None))
         assert not instrument.is_ready()
 
-    def test_opentelemetry_includes_pyroscope(self) -> None:
-        otel_instrument = OpentelemetryInstrument(
-            OpentelemetryConfig(pyroscope_endpoint=pydantic.HttpUrl("http://localhost:4040"))
+    def test_opentelemetry_includes_pyroscope_2(
+        self, monkeypatch: pytest.MonkeyPatch, minimal_opentelemetry_config: OpentelemetryConfig
+    ) -> None:
+        monkeypatch.setattr("opentelemetry.sdk.trace.TracerProvider.shutdown", Mock())
+        monkeypatch.setattr("pyroscope.add_thread_tag", add_thread_tag_mock := Mock())
+        monkeypatch.setattr("pyroscope.remove_thread_tag", remove_thread_tag_mock := Mock())
+
+        minimal_opentelemetry_config.pyroscope_endpoint = pydantic.HttpUrl("http://localhost:4040")
+
+        opentelemetry_instrument: typing.Final = FastApiOpentelemetryInstrument(minimal_opentelemetry_config)
+        opentelemetry_instrument.bootstrap()
+        fastapi_application: typing.Final = opentelemetry_instrument.bootstrap_after(fastapi.FastAPI())
+
+        @fastapi_application.get("/test-handler")
+        async def test_handler() -> None: ...
+
+        FastAPITestClient(app=fastapi_application).get("/test-handler")
+
+        assert (
+            add_thread_tag_mock.mock_calls
+            == remove_thread_tag_mock.mock_calls
+            == [mock.call(mock.ANY, "span_id", mock.ANY), mock.call(mock.ANY, "span_name", "GET /test-handler")]
         )
-        otel_instrument.bootstrap()
-        assert PyroscopeSpanProcessor in {
-            type(one_span_processor)
-            for one_span_processor in otel_instrument.tracer_provider._active_span_processor._span_processors  # noqa: SLF001
-        }
