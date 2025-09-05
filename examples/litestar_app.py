a63eb5b0705b987b51301903abfa4905aa3c8189
Lev Vereshchagin <mail@vrslev.com>
2025-09-05 16:59:42 +0300

    Clean up unused imports and simplify app creation in litestar example

--- examples/litestar_app.py
+++ examples/litestar_app.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
-import logging
-import random
 
 import litestar
-import structlog
 
 from microbootstrap import LitestarSettings
 from microbootstrap.bootstrappers.litestar import LitestarBootstrapper
@@ -14,34 +11,18 @@ from microbootstrap.granian_server import create_granian_server
 class Settings(LitestarSettings): ...
 
 
-settings = Settings(service_debug=False)
+settings = Settings()
 
 
 @litestar.get("/")
 async def hello_world() -> dict[str, str]:
-    # 1/0
-    structlog.get_logger().info("fdsjkfl")
-    structlog.get_logger().error("testing structlog sentry integration", rand=random.gauss(0, 1))
     return {"hello": "world"}
-
-
-@litestar.get("/2")
-async def hello_world2() -> dict[str, str]:
-    # 1/0
-    logging.getLogger().info("testing structlog sentry integration")
-    logging.getLogger().critical("testing structlog sentry integration")
-    return {"hello": "world"}
-
-
-b: LitestarBootstrapper
 
 
 def create_app() -> litestar.Litestar:
-    global b
-    b = LitestarBootstrapper(settings)
-    t = b.configure_application(LitestarConfig(route_handlers=[hello_world, hello_world2])).bootstrap()
-    # structlog.get_logger().error("testing structlog sentry integration")
-    return t
+    return (
+        LitestarBootstrapper(settings).configure_application(LitestarConfig(route_handlers=[hello_world])).bootstrap()
+    )
 
 
 if __name__ == "__main__":
