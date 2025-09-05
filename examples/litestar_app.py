1e53ca9a1b3be3184d1f73a86ccca5d038fdf262
Lev Vereshchagin <mail@vrslev.com>
2025-09-05 16:24:10 +0300

    Add structlog integration and debug changes to litestar example

--- examples/litestar_app.py
+++ examples/litestar_app.py
@@ -1,28 +1,40 @@
 from __future__ import annotations
+import logging
 
 import litestar
+import structlog
 
 from microbootstrap import LitestarSettings
 from microbootstrap.bootstrappers.litestar import LitestarBootstrapper
 from microbootstrap.config.litestar import LitestarConfig
 from microbootstrap.granian_server import create_granian_server
-
+import random
 
 class Settings(LitestarSettings): ...
 
 
-settings = Settings()
-
+settings = Settings( service_debug=False, sentry_additional_params={"debug":True})
 
 @litestar.get("/")
 async def hello_world() -> dict[str, str]:
+    # 1/0
+    structlog.get_logger().info("fdsjkfl")
+    structlog.get_logger().error("testing structlog sentry integration", rand=random.gauss(0,1))
+    return {"hello": "world"}
+@litestar.get("/2")
+async def hello_world2() -> dict[str, str]:
+    # 1/0
+    logging.getLogger().info("testing structlog sentry integration")
+    logging.getLogger().critical("testing structlog sentry integration")
     return {"hello": "world"}
 
-
+b: LitestarBootstrapper
 def create_app() -> litestar.Litestar:
-    return (
-        LitestarBootstrapper(settings).configure_application(LitestarConfig(route_handlers=[hello_world])).bootstrap()
-    )
+    global b
+    b = LitestarBootstrapper(settings)
+    t = b.configure_application(LitestarConfig(route_handlers=[hello_world, hello_world2])).bootstrap()
+    # structlog.get_logger().error("testing structlog sentry integration")
+    return t
 
 
 if __name__ == "__main__":
