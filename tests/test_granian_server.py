00397fe94a2470e31bdb434961e5e347c38e6fd1
Lev Vereshchagin <mail@vrslev.com>
2026-08-21 17:28:18 +0300

    Allow configuring Granian interface (#143)
    * Allow configuring Granian interface
    
    * Exclude README from Ruff formatting
    
    * Ignore new Ruff baseline rules

--- tests/test_granian_server.py
+++ tests/test_granian_server.py
@@ -1,8 +1,18 @@
 import granian
+from granian.constants import Interfaces
 
 from microbootstrap.granian_server import create_granian_server
 from microbootstrap.settings import ServerConfig
 
 
 def test_granian_server(minimal_server_config: ServerConfig) -> None:
-    assert isinstance(create_granian_server("some:app", minimal_server_config), granian.Granian)
+    server = create_granian_server("some:app", minimal_server_config)
+
+    assert isinstance(server, granian.Granian)
+    assert server.interface is Interfaces.ASGI
+
+
+def test_granian_server_custom_interface(minimal_server_config: ServerConfig) -> None:
+    server = create_granian_server("some:app", minimal_server_config, interface=Interfaces.ASGINL)
+
+    assert server.interface is Interfaces.ASGINL
